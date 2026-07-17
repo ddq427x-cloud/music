de1507f33d9fbb054fc9cf79105370a46ef1e965
ddq427x-cloud <ddq427x@gmail.com>
2026-07-17 11:31:43 +0000

    Clean temporary build files

--- kuolao-music-all-in-one/migrations/0004_source_autoprobe.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE music_sources ADD COLUMN source_kind TEXT NOT NULL DEFAULT 'template';
-ALTER TABLE music_sources ADD COLUMN adapter_config TEXT NOT NULL DEFAULT '{}';
-CREATE INDEX IF NOT EXISTS idx_music_sources_kind ON music_sources(user_id, source_kind, platform);
